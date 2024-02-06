4a787e3bbce483e7564c117d9fbee90549770a68
Damian Peiris. <drpeiris3@gmail.com>
2024-02-06 11:00:27 +0530

    Installed MySql.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import express,{Request,Response} from 'express';
 const app = express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!')
+})
+
+
+app.get('/',(req,res)=>{
+    res.json({msg : "Hello!"})
+
 })
