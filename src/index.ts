8e4b381dc3b0d864c41d60a975d0bea99e032374
Damian Peiris. <drpeiris3@gmail.com>
2024-02-06 11:40:09 +0530

    Optimized the manual DB configuration process.

--- src/index.ts
+++ src/index.ts
@@ -5,57 +5,55 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-const dbConfig = require("./SqlConfig/MySqlConfig");
+
 const app = express();
-const connection = mysql.createConnection(dbConfig);
-
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
-connection.connect((err: Error) => {
-  if (err) {
-    console.log(err.message);
-    
-  } else {
-    console.log("Connected to the database!");
-    rl.question("Enter the host : ",(hostname:string)=>{
-        rl.question("Enter the user : ",(username:string)=>{
-            rl.question("Enter the password : ",(pw:string)=>{
-                rl.question("Enter the database name : ",(dbName:string)=>{
-                    const dbConfig = {
-                        host : hostname,
-                        user : username,
-                        password:pw,
-                        database:dbName
-                    }
-                    connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`,(er,result)=>{
-                        if(er){
-                            console.log(er.message);
 
-                        }else{
-                            console.log("Database created successfully!");
-                            connection.query(`USE ${dbName}`,(e,r)=>{
-                                if(e){
-                                    console.log(e.message);
-                                }else{
-                                    console.log("Database selected successfully!");
-                                }
-                            })
-                        }
-                    })
+rl.question("Enter the host : ",(hostname:string)=>{
+    rl.question("Enter the user : ",(username:string)=>{
+        rl.question("Enter the password : ",(pw:string)=>{
+            rl.question("Enter the database name : ",(dbName:string)=>{
+                const dbConfig = {
+                    host : hostname,
+                    user : username,
+                    password:pw,
+                    database:dbName
+                }
+                const conncetion = mysql.createConnection(dbConfig);
+                conncetion.connect((er:Error)=>{
+                    if(er)console.log('An error occured while connecting to the database : ',er.message);
+                    else console.log('Connected to the database successfully!');
+                    createAndUseDatabase(dbName,conncetion);
 
                 })
+                
 
             })
 
         })
 
-        
     })
-  }
-});
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello!" });
-});
+    
+})
+
+function createAndUseDatabase(dbName :string,connection:mysql.Connection){
+    connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`,(er,result)=>{
+        if(er){
+            console.log(er.message);
+
+        }else{
+            console.log("Database created successfully!");
+            connection.query(`USE ${dbName}`,(e,r)=>{
+                if(e){
+                    console.log(e.message);
+                }else{
+                    console.log("Database selected successfully!");
+                }
+            })
+        }
+    })
+
+}
